543a5e84fd9cf5b46bcc29e57e55d7bd1720a274
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-04-30 21:33:21 -0700

    tried a different approach

--- backend/stream/utils/db_util.py
+++ backend/stream/utils/db_util.py
@@ -1,4 +1,9 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, String
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from backend.stream.utils import file_util
 
 
 def create_table(table_name, engine):
@@ -14,10 +19,24 @@ def create_table(table_name, engine):
     return table
 
 
-def update_article_url_db(article_dicts, table_name, db_file_path):
+def update_article_url_db(article_dicts, publisher, db_file_path):
+    file_util.make_parent_dirs(db_file_path)
     engine = create_engine('sqlite:///{}'.format(db_file_path), echo=True)
-    table = create_table(table_name, engine)
-    with engine.connect() as connection:
-        for article_dict in article_dicts:
-            statement = table.update().values(article_dict)
-            result = connection.execute(statement)
+
+    class Article(declarative_base()):
+        __tablename__ = publisher
+
+        url = Column(String, primary_key=True)
+        title = Column(String)
+        publishedAt = Column(String)
+        addedAt = Column(String)
+
+    articles = [Article(**article_dict) for article_dict in article_dicts]
+    session = sessionmaker(bind=engine)()
+    try:
+        session.add_all(articles)
+        session.commit()
+    except SQLAlchemyError as e:
+        print(e)
+    finally:
+        session.close()
