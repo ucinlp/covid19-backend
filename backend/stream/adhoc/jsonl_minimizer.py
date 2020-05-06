a51930f7eacd5e2c47ca088843e8ec15320f5d43
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-05-05 19:00:45 -0700

    added tweet filter

--- backend/stream/adhoc/jsonl_minimizer.py
+++ backend/stream/adhoc/jsonl_minimizer.py
@@ -11,6 +11,8 @@ def get_argparser():
     parser = argparse.ArgumentParser(description='Jsonl minimizer')
     parser.add_argument('--input', required=True, help='input dir/file path')
     parser.add_argument('--config', required=True, help='config file path')
+    parser.add_argument('--lang', required=False, help='target language')
+    parser.add_argument('-include_rt', action='stored_true', help='include retweets')
     parser.add_argument('--output', required=True, help='output dir/file path')
     return parser
 
@@ -33,12 +35,19 @@ def write_jsonl_file(json_objs, output_file_path, first=True):
             fp.write('{}\n'.format(json.dumps(json_obj)))
 
 
-def process_single_jsonl(input_file_path, config, output_file_path, buffer_size=10000):
+def process_single_jsonl(input_file_path, config, target_lang, includes_rt, output_file_path, buffer_size=10000):
     buffer_list = list()
     first = True
     with open(input_file_path, 'r') as fp:
         for line in fp:
             main_dict = json.loads(line)
+            if target_lang is not None and main_dict.get('lang', None) != target_lang:
+                continue
+            elif not includes_rt:
+                retweeted_status = main_dict.get('retweeted_status', None)
+                if retweeted_status is None or len(retweeted_status) == 0:
+                    continue
+
             sub_dict = build_sub_dict(main_dict, config)
             buffer_list.append(sub_dict)
             if len(buffer_list) >= buffer_size:
@@ -48,25 +57,27 @@ def process_single_jsonl(input_file_path, config, output_file_path, buffer_size=
         write_jsonl_file(buffer_list, output_file_path, first=first)
 
 
-def process_multiple_jsonls(input_dir_path, config, output_dir_path):
+def process_multiple_jsonls(input_dir_path, config, target_lang, includes_rt, output_dir_path):
     for input_file_path in get_file_paths(input_dir_path):
         output_file_path = os.path.join(output_dir_path, os.path.basename(input_file_path))
-        process_single_jsonl(input_file_path, config, output_file_path)
+        process_single_jsonl(input_file_path, config, target_lang, includes_rt, output_file_path)
 
 
 def main(args):
     with open(args.config, 'r') as fp:
         config = yaml.load(fp, Loader=yaml.FullLoader)
 
-    is_file = os.path.isfile(args.input)
+    input_path = args.input
+    includes_rt = args.include_rt
+    is_file = os.path.isfile(input_path)
     if is_file:
-        process_single_jsonl(args.input, config, args.output)
+        process_single_jsonl(input_path, config, args.lang, includes_rt, args.output)
     else:
-        input_dir_paths = get_dir_paths(args.input)
+        input_dir_paths = get_dir_paths(input_path)
         output_root_dir_path = args.output
         for input_dir_path in input_dir_paths:
             output_dir_path = os.path.join(output_root_dir_path, os.path.basename(input_dir_path))
-            process_multiple_jsonls(input_dir_path, config, output_dir_path)
+            process_multiple_jsonls(input_dir_path, config, args.lang, includes_rt, output_dir_path)
 
 
 if __name__ == '__main__':
