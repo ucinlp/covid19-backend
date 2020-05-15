4e611de0a3598233776e1daf86ff6726e877066c
rloganiv <rloganiv@gmail.com>
2020-05-14 21:17:57 -0700

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='covid19-backend',
+    version='0.0.1',
+    description='Backend for UCI COVID-19 misinformation detection project.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ucinlp/covid19-backend',
+    packages=find_packages(exclude=('tests', 'scripts')),
+    python_requires='>=3.6',
+    install_requires=[
+        'flask>=1.0.2',
+        'flask-cors>=3.0.0',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'gunicorn>=20.0.4',
+        'overrides>=2.8.0',
+        'transformers[torch]>=2.7.0',
+    ],
+    extras_require={
+        'test': ['flake8', 'pytest']
+    },
+)
