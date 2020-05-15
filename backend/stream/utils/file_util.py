0e85b0b0fa7bd96bb413a49565149bc9dabdb998
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-05-15 14:20:53 -0700

    fixed bugs

--- backend/stream/utils/file_util.py
+++ backend/stream/utils/file_util.py
@@ -1,11 +1,14 @@
 import os
 
 
-def get_dir_paths(dir_path):
+def get_dir_paths(dir_path, ignored_set=None):
+    if ignored_set is None:
+        ignored_set = {'__pycache__'}
+
     file_path_list = list()
     for file_name in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file_name)
-        if os.path.isdir(file_path) and not file_name.startswith('.'):
+        if os.path.isdir(file_path) and not file_name.startswith('.') and file_name not in ignored_set:
             file_path_list.append(file_path)
     return file_path_list
 
--- tests/stream_utils_test.py
+++ tests/stream_utils_test.py
@@ -11,14 +11,14 @@ class FileUtilTest(TestCase):
 
     def test_get_file_paths(self):
         expected_list = ['tests/fixtures/misconceptions.jsonl']
-        file_paths = file_util.get_dir_paths('tests/fixtures/')
+        file_paths = file_util.get_file_paths('tests/fixtures/')
         assert file_paths == expected_list
 
 
 class MiscUtilTest(TestCase):
     def test_overwrite_dict(self):
         org_dict = {'test': 'yay', 10: ['hello', 'world']}
-        sub_dict = {'test': 'wow', '10': ['should not overwrite']}
+        sub_dict = {'test': 'wow', 10: ['hello', 'world'], '10': ['should not overwrite']}
         expected_dict = {'test': 'wow', '10': ['should not overwrite']}
         misc_util.overwrite_dict(org_dict, sub_dict)
         assert org_dict == expected_dict
@@ -26,6 +26,6 @@ class MiscUtilTest(TestCase):
     def test_overwrite_config(self):
         config = {'test': 'yay', 10: ['hello', 'world']}
         json_str = '{"test": "wow", "10": ["should not overwrite"]}'
-        expected_dict = {'test': 'wow', '10': ['should not overwrite']}
+        expected_dict = {'test': 'wow', 10: ['hello', 'world'], '10': ['should not overwrite']}
         misc_util.overwrite_config(config, json_str)
         assert config == expected_dict
