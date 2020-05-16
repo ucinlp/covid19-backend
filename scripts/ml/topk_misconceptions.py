8ad26fa2aa5ad84b360ea62c4f43b95b4ee30a08
rloganiv <rloganiv@gmail.com>
2020-05-16 15:11:04 -0700

    Moved script used to identify top-k tweets/misconception into scripts

--- scripts/ml/topk_misconceptions.py
+++ scripts/ml/topk_misconceptions.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 from heapq import heapify, heappush, heappushpop
 import json
 
@@ -26,8 +27,8 @@ class MaxHeap:
 
 
 def generate_sentences(fname,
-                       batch_size=10,
-                       field='text'):
+                       batch_size=8,
+                       field='full_text'):
     with open(fname, 'r') as f:
         batch = []
         for line in f:
@@ -41,12 +42,15 @@ def generate_sentences(fname,
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, help='input data')
+    parser.add_argument('--output', type=str, help='output data')
     parser.add_argument('--misconceptions', type=str, help='JSONL file containing misconceptions')
     parser.add_argument('--score_type', type=str, choices=('precision', 'recall', 'f1'), default='f1')
     parser.add_argument('--k', type=int, default=5)
     args = parser.parse_args()
 
-    detector = BertScoreDetector('bert-base-uncased', score_type=args.score_type)
+    detector = BertScoreDetector('covid-roberta/checkpoint-84500', score_type=args.score_type)
+    detector.eval()
+    detector.cuda()
 
     with open(args.misconceptions, 'r') as f:
         misconceptions = MisconceptionDataset.from_jsonl(f)
@@ -63,10 +67,19 @@ def main():
                 x = (scores[sentence_idx, misconception_idx], sentences[sentence_idx])
                 top_scoring_tweets[misconception_idx].push(x)
 
-    for i, misconception in enumerate(misconceptions.sentences):
-        print(f'Misconception: {misconception}')
-        for score, sentence in top_scoring_tweets[i].view():
-            print(f'\t{score} :: {sentence}')
+    with open(args.output, 'w') as f:
+        fieldnames = ['id', 'pos_variation', 'tweet', 'score']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for i, pos_variation in enumerate(misconceptions.sentences):
+            id_ = misconceptions[i].id
+            for score, tweet in top_scoring_tweets[i].view():
+                writer.writerow({
+                    'id': id_,
+                    'pos_variation': pos_variation,
+                    'tweet': tweet,
+                    'score': score
+                })
 
 
 if __name__ == '__main__':
