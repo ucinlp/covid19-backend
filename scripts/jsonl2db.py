06c0c48c9d48a3e3ddfe0a7f36542f39fdcd6676
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-05-24 21:50:01 -0700

    added jsonl2db

---
+++ scripts/jsonl2db.py
@@ -0,0 +1,30 @@
+import argparse
+import json
+from pathlib import Path
+
+from backend.stream.db.operation import add_entities
+from backend.stream.db.util import get_engine
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='JSONL to DB')
+    parser.add_argument('--input', required=True, type=lambda p: Path(p), help='dictionary to overwrite config')
+    parser.add_argument('--table', required=True, help='table class name')
+    parser.add_argument('--db', required=True, type=lambda p: Path(p), help='output DB file path')
+    return parser
+
+
+def add_records(records, table_class_name, db_file_path):
+    engine = get_engine(db_file_path)
+    add_entities(records, engine, table_class_name)
+
+
+def main(args):
+    with open(args.input, 'r') as fp:
+        records = [json.loads(line) for line in fp]
+    add_records(records, args.table, args.db)
+
+
+if __name__ == '__main__':
+    argparser = get_argparser()
+    main(argparser.parse_args())
