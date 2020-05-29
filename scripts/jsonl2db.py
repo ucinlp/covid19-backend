1d597235fa629f48e6c6109ff2340606d7f3c56f
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-05-29 14:16:13 -0700

    extended jsonl2db

--- scripts/jsonl2db.py
+++ scripts/jsonl2db.py
@@ -10,8 +10,19 @@ def get_argparser():
     parser = argparse.ArgumentParser(description='JSONL to DB')
     parser.add_argument('--input', required=True, type=lambda p: Path(p), help='input JSONL file path')
     parser.add_argument('--table', required=True, help='table class name')
+    parser.add_argument('--custom', help='modify records before adding, for a specific type of input file ')
     parser.add_argument('--db', required=True, type=lambda p: Path(p), help='output DB file path')
     return parser
+
+
+def modify_records(custom_type, records):
+    if custom_type == 'initial_wiki_ver':
+        for i in range(records):
+            old_record = records[i]
+            record = {'id': old_record.pop('id'), 'text': old_record.pop('canonical_text'),
+                      'source': old_record.pop('origin'), 'reliability': old_record.pop('reliability_score'),
+                      'url': json.dumps({'list': old_record.pop('sources')}), 'misc': json.dumps(old_record)}
+            records[i] = record
 
 
 def add_records(records, table_class_name, db_file_path):
@@ -22,6 +33,9 @@ def add_records(records, table_class_name, db_file_path):
 def main(args):
     with open(args.input, 'r') as fp:
         records = [json.loads(line) for line in fp]
+
+    if args.custon is not None:
+        modify_records(args.custom, records)
     add_records(records, args.table, args.db)
 
 
