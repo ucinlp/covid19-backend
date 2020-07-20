c6edb47380fd831d58e41a7503f6200080c73b93
rloganiv <rloganiv@gmail.com>
2020-07-19 19:07:08 -0700

    Rolled back transformers version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'google-auth-oauthlib',
         'gunicorn>=20.0.4',
         'overrides>=2.8.0',
-        'transformers[torch]>=2.7.0',
+        'transformers[torch]==2.7.0',  # TODO: Upgrade to version 3.
         'sqlalchemy>=1.3.16'
     ],
     extras_require={
